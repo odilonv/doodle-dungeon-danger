61eb4bb158c6e404f91784f8f2834a846f148339
Raphael Dimeck <raphael.dimeck@gmail.com>
2025-02-03 17:18:40 +0100

    Add heroes table to database schema

--- backend/assets/createTables.sql
+++ backend/assets/createTables.sql
@@ -6,28 +6,16 @@ CREATE TABLE IF NOT EXISTS `user` (
     password VARCHAR (255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `board` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(id)
-);
-
-CREATE TABLE IF NOT EXISTS `card` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    board_id INT,
-    user_id INT,
-    state INT DEFAULT 1,
-    FOREIGN KEY (board_id) REFERENCES board(id),
-    FOREIGN KEY (user_id) REFERENCES user(id)
-);
-
 CREATE TABLE IF NOT EXISTS `user_validation` (
     user_id INT,
     email_token VARCHAR(255),
     validated BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
+CREATE TABLE IF NOT EXISTS `heroes` {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    image VARCHAR(255) NOT NULL
+}
